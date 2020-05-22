127b415ea6c0ed16b1b381a36f5914eb2d4d9ab6
hong-dev <jjuggumih@gmail.com>
2020-05-23 00:34:00 +0900

    create todo list view 1. 메인 화면으로 등록되어 있는 todo list가 보이도록 view 작성     * 먼저 등록한 순서대로 보이도록 정렬     * active와 done을 query parameter로 받아서 filtering 2. templates 폴더에 index.html 생성 후 view에서 render 되도록 설정

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title      = models.CharField(max_length = 100)
+    image      = models.URLField(null = True)
+    is_active  = models.BooleanField(default = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'todos'
