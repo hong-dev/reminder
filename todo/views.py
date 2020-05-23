3755f3bf093ca14823c2e6d4006bc6187b08b1b8
hong-dev <jjuggumih@gmail.com>
2020-05-23 17:07:16 +0900

    create todo create, delete, change status views 1. 할 일 추가하는 view 2. 할 일 삭제하는 view 3. 할 일의 완료여부 변경하는 view

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from .models import Todo
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views     import View
 
 class TodoListView(View):
@@ -18,7 +18,34 @@ class TodoListView(View):
             .objects
             .filter(**filter_condition)
             .order_by('created_at')
-            .values('title', 'image', 'is_active', 'created_at')
+            .values('id', 'title', 'image', 'is_active', 'created_at')
         )
 
-        return render(request, 'todo/index.html', {'todo_list': list(todo_list)})
+        return render(request, 'todo/index.html', {'todo_list': todo_list})
+
+class TodoCreateView(View):
+    def post(self, request):
+        title = request.POST.get('title', 'Untitled')
+        image = request.POST.get('image')
+
+        Todo.objects.create(
+            title     = title,
+            image     = image,
+            is_active = True
+        )
+
+        return redirect('/')
+
+class TodoDeleteView(View):
+    def post(self, request, todo_id):
+        Todo.objects.get(id = todo_id).delete()
+
+        return redirect('/')
+
+class TodoStatusView(View):
+    def post(self, request, todo_id):
+        todo = Todo.objects.get(id = todo_id)
+        todo.is_active = True if todo.is_active == False else False
+        todo.save()
+
+        return redirect('/')
